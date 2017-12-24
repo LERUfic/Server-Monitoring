9da77995657dc66c360823a931fcc224e988b5b0
Unknown <aguelsw@gmail.com>
2017-12-24 08:10:09 +0700

    AJAX Implementation
    Change the manual "hide and show" javascript into using AJAX to make it more dynamic

---
+++ controller/c_body.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $( "#divData" ).load( "/root/views/partials/body.ejs #dashboard" );
+    console.log("masuk");
+});
+
+$('#invenList').click(function(){changeListBarang();return false;});
+$('#invenAdd').click(function(){changeTambahBarang();return false;});
+$('#resList').click(function(){changeListPeminjam();return false;});
+$('#resRes').click(function(){changeReservasi();return false;});
+
+function changeListBarang(){
+  $( "#divData" ).load( "/root/views/partials/body.ejs #list_barang" );
+}
+
+function changeTambahBarang(){
+  $( "#divData" ).load( "/root/views/partials/body.ejs #tambah_barang" );
+}
+
+function changeListPeminjam(){
+  $( "#divData" ).load( "/root/views/partials/body.ejs #list_peminjam" );
+}
+
+function changeReservasi(){
+  $( "#divData" ).load( "/root/views/partials/body.ejs #reservasi" );
+}
